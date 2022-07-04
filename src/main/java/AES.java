31160f3ee73c59cb02032d5901ed5aec330f292b
Priyadarshan <developer.priyadarshan@gmail.com>
2022-07-04 23:43:18 +0530

    key to string conversion

--- src/main/java/AES.java
+++ src/main/java/AES.java
@@ -1,7 +1,10 @@
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class AES {
 
@@ -10,5 +13,17 @@ public class AES {
 	    keyGenerator.init(n);
 	    SecretKey key = keyGenerator.generateKey();
 	    return key;
+	}
+	
+	public static String keyToString(Key key) {
+		String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
+		return encodedKey;
+	}
+	
+	public static Key stringToKey(String encodedKey){
+		byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+		// rebuild key using SecretKeySpec
+		SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+		return originalKey; 
 	}
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,21 @@
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.SecretKey;
+
 public class Main {
 
-	public static void main(String[] args) {}
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		AES aes = new AES();
+		Key key = AES.generateKey(256);
+		String encodedKeyString = aes.keyToString(key);
+		System.out.println("Base64 Encoded Key String : " + encodedKeyString);
+		
+		Key originalKey = aes.stringToKey(encodedKeyString);
+		
+		if(originalKey.equals(key))
+			System.out.println(true);
+		else
+			System.out.println(false);
+	}
 }
